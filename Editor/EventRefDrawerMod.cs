51991f8263d2b5f0b83c7be11829a4680ce85c5e
Made Indrayana <indra@doubleshot-audio.com>
2021-05-18 23:43:26 +0200

    New implementation with search string instead of external file injection. NAISU

--- Editor/EventRefDrawerMod.cs
+++ Editor/EventRefDrawerMod.cs
@@ -26,6 +26,47 @@ using UnityEngine;
 
 namespace DoubleShot.Editor
 {
+    public class PatchContent
+    {
+        public const string EventRefDrawerSearchString = "pathProperty.stringValue = ((EditorEventRef)DragAndDrop.objectReferences[0]).Path;";
+        public const string EventRefDrawerReplaceString = @"
+                    if(EditorPrefs.GetBool(""DoubleShot.FMODGUIDTweak.Enabled"", false))
+                        pathProperty.stringValue = ""{"" + ((EditorEventRef) DragAndDrop.objectReferences[0]).Guid.ToString() + ""}"";
+                    else
+                        pathProperty.stringValue = ((EditorEventRef) DragAndDrop.objectReferences[0]).Path;";
+
+
+
+        public const string SwapperSearch = "Rect foldoutRect = new Rect(position.x + 10, position.y + baseHeight, position.width, baseHeight);";
+        public const string SwapperReplace = @"
+
+                #region Added the swap GUID/Path functionality
+                
+                // Tested and verified with FMOD 2.00.08 to 2.01.07
+                Rect swapRect = new Rect(searchRect.x, position.y + baseHeight, searchRect.width * 3 + 5, baseHeight);
+                if (GUI.Button(swapRect, new GUIContent(""Swap""), buttonStyle) && !string.IsNullOrEmpty(pathProperty.stringValue) && ((EventManager.EventFromPath(pathProperty.stringValue) != null) || EventManager.EventFromPath(pathProperty.stringValue) != null))
+                {
+                    EditorEventRef eventRef = EventManager.EventFromPath(pathProperty.stringValue);
+                    if (pathProperty.stringValue.StartsWith(""{""))
+                    {
+                        property.stringValue = eventRef.Path;
+                        property.serializedObject.ApplyModifiedProperties();
+                        GUI.changed = true;
+                    }
+                    else
+                    {
+                        property.stringValue = eventRef.Guid.ToString(""b"");
+                        property.serializedObject.ApplyModifiedProperties();
+                        GUI.changed = true;
+                    }
+                }
+                
+                #endregion
+
+
+                Rect foldoutRect = new Rect(position.x + 10, position.y + baseHeight, position.width, baseHeight);";
+    }
+
     public class EventRefDrawerMod
     {
         [MenuItem("Double Shot/FMOD/Patcher/Event to GUID Swapper", false, 0)]
@@ -41,6 +82,7 @@ namespace DoubleShot.Editor
                 return;
             }
 
+            /*
             // Null checker for PatchContent, accounting for either Package install or .unitypackage install
             if (!File.Exists(injectionPath))
             {
@@ -53,16 +95,19 @@ namespace DoubleShot.Editor
                 else
                     injectionPath = Path.Combine(Application.dataPath, "DoubleShot/Editor/EventRefDrawerPatchContent.md");
             }
+            
 
             StringBuilder tempNewScript = new StringBuilder();
             StreamReader file = new StreamReader(path, true);
             string line;
+            */
 
             if (EditorUtility.DisplayDialog("Double Shot Audio Patcher",
                 "This FMOD patch will add a \"Swap\" button to [FMODUnity.EventRef] property which facilitates swapping Event Path with GUID. \n\n" +
                 "Patch has been tested and verified in FMOD Version 2.00.08 up to 2.01.07. \n\n" +
                 "Do you want to continue?", "Yes", "No"))
             {
+                /*
                 // Checks if patch is already there
                 while ((line = file.ReadLine()) != null)
                 {
@@ -77,27 +122,24 @@ namespace DoubleShot.Editor
 
                 file.DiscardBufferedData();
                 file.BaseStream.Seek(0, SeekOrigin.Begin);
+                file.Close();
+                */
 
                 #region NEW IMPLEMENTATION HERE:
-                /*
+
+                EditorPrefs.SetBool("DoubleShot.FMODGUIDTweak.Enabled", false);
 
                 // reading all the file and then put it into temp string
-                string[] tempTxt = File.ReadAllLines(path);
+                string temp = File.ReadAllText(path);
+                temp = temp.Replace(PatchContent.EventRefDrawerSearchString, PatchContent.EventRefDrawerReplaceString);
+                temp = temp.Replace(PatchContent.SwapperSearch, PatchContent.SwapperReplace);
+                Debug.Log(temp);
 
-                // search the string array for the one you want to search 
-                int pos = ArrayUtility.IndexOf<string>(tempTxt, "                    pathProperty.stringValue = ((EditorEventRef)DragAndDrop.objectReferences[0]).Path;");
-                
-                if(pos != -1) // if found, do replace stuffs
-                {
-                    tempTxt[pos] = "                    Debug.Log(\"Success\");";
-                }
+                File.WriteAllText(path, temp);
 
-                // and then write all changes at once.
-                File.WriteAllLines(path, tempTxt);
-
-                */
                 #endregion
 
+                /*
                 do
                 {
                     line = file.ReadLine();
@@ -117,6 +159,7 @@ namespace DoubleShot.Editor
                 StreamWriter writer = new StreamWriter(path);
                 writer.Write(tempNewScript);
                 writer.Close();
+                */
 
                 AssetDatabase.ImportAsset("Assets/Plugins/FMOD/src/Editor/EventRefDrawer.cs");
 
@@ -132,7 +175,7 @@ namespace DoubleShot.Editor
         
         public static bool isTweakEnabled
         {
-            get { return EditorPrefs.GetBool(SettingName, true); }
+            get { return EditorPrefs.GetBool(SettingName, false); }
             set { EditorPrefs.SetBool(SettingName, value); }
         }
 
@@ -148,5 +191,6 @@ namespace DoubleShot.Editor
             Menu.SetChecked(MenuName, isTweakEnabled);
             return true;
         }
-    
+
+    }
 }
