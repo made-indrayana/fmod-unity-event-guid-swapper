e4f8114c20a30b1e6305adaf5e9abc65030c86c6
Made Indrayana <indra@doubleshot-audio.com>
2021-05-19 16:17:28 +0200

    Renamed file and polished everything.

--- Editor/Patcher.cs
+++ Editor/Patcher.cs
@@ -15,7 +15,7 @@
         ------------------------------------------------------------------------------------------------------------------------------
 */
 
-// FMOD EventRefDrawer.cs Patcher
+// FMOD Event to GUID Workflow Swapper Patcher
 // by Made Indrayana - Double Shot Audio
 // Execute from Menu "Double Shot/FMOD/Patcher/Event to GUID Swapper"
 
@@ -34,10 +34,10 @@ namespace DoubleShot.Editor
         public const string PatchReference = "Using System;";
         public const string PatchReferenceAppend = "// Patched by Made Indrayana using FMOD EventRefDrawer.cs Patcher - https://github.com/made-indrayana/fmod-unity-event-guid-swapper \r\n";
 
-        public const string EventRefDrawerSearchString = "pathProperty.stringValue = ((EditorEventRef)DragAndDrop.objectReferences[0]).Path;";
-        public const string EventRefDrawerReplaceString = @"
+        public const string DrawerSearch = "pathProperty.stringValue = ((EditorEventRef)DragAndDrop.objectReferences[0]).Path;";
+        public const string DrawerReplace = @"
                     if (EditorPrefs.GetBool(""DoubleShot.FMODGUIDTweak.Enabled""))
-                        pathProperty.stringValue = ""{"" + ((EditorEventRef) DragAndDrop.objectReferences[0]).Guid.ToString() + ""}"";
+                        pathProperty.stringValue = ((EditorEventRef) DragAndDrop.objectReferences[0]).Guid.ToString(""b"");
                     else
                         pathProperty.stringValue = ((EditorEventRef) DragAndDrop.objectReferences[0]).Path;";
 
@@ -69,26 +69,79 @@ namespace DoubleShot.Editor
 
                 Rect foldoutRect = new Rect(position.x + 10, position.y + baseHeight, position.width, baseHeight);";
 
-        public const string EventBrowserSearchStringOne = "string path = (data as EditorEventRef).Path;";
-        public const string EventBrowserReplaceStringOne = @"
+        public const string BrowserSearchOne = "string path = (data as EditorEventRef).Path;";
+        public const string BrowserReplaceOne = @"
                     string path;
                     if (EditorPrefs.GetBool(""DoubleShot.FMODGUIDTweak.Enabled""))
-                        path = ""{"" + (data as EditorEventRef).Guid.ToString() + ""}"";
+                        path = (data as EditorEventRef).Guid.ToString(""b"");
                     else
                         path = (data as EditorEventRef).Path;";
 
-        public const string EventBrowserSearchStringTwo = "emitter.Event = (data as EditorEventRef).Path;";
-        public const string EventBrowserReplaceStringTwo = @"
+        public const string BrowserSearchTwo = "emitter.Event = (data as EditorEventRef).Path;";
+        public const string BrowserReplaceTwo = @"
                         if (EditorPrefs.GetBool(""DoubleShot.FMODGUIDTweak.Enabled""))
-                            emitter.Event = ""{"" + (data as EditorEventRef).Guid.ToString() + ""}"";
+                            emitter.Event = (data as EditorEventRef).Guid.ToString(""b"");
                         else
                             emitter.Event = (data as EditorEventRef).Path;";
+
+        public const string BrowserSearchThree = @"                if (itemIDs.TryGetValue(path, out itemID))
+                {
+                    SetSelection(new List<int> { itemID },
+                        TreeViewSelectionOptions.RevealAndFrame | TreeViewSelectionOptions.FireSelectionChanged);
+                }
+                else
+                {
+                    SetSelection(new List<int>());
+                }";
+        public const string BrowserReplaceThree = @"
+                if (itemIDs.TryGetValue(path, out itemID))
+                {
+                    SetSelection(new List<int> { itemID },
+                        TreeViewSelectionOptions.RevealAndFrame | TreeViewSelectionOptions.FireSelectionChanged);
+                }
+                else if (itemIDs.TryGetValue(EventManager.EventFromPath(path).name.ToString(), out itemID))
+                {
+                    SetSelection(new List<int> { itemID },
+                        TreeViewSelectionOptions.RevealAndFrame | TreeViewSelectionOptions.FireSelectionChanged);
+                }
+                else
+                {
+                    SetSelection(new List<int>());
+                }";
+
     }
 
-    public class EventRefDrawerMod
+    [InitializeOnLoad]
+    public class EditorStart
     {
-        [MenuItem("Double Shot/FMOD/Patcher/Event to GUID Swapper", false, 0)]
-        public static void EventRefDrawerPatcher()
+        static EditorStart()
+        {
+            string eventRefDrawerPath = Path.Combine(Application.dataPath, PatchContent.EventRefDrawerPath);
+
+            // Check if swap patch is already applied
+            string check = File.ReadAllText(eventRefDrawerPath);
+            if (!check.Contains(PatchContent.PatchReferenceAppend))
+            {
+                Debug.LogWarning("Event to GUID Patch is not active! To apply/reapply, go to Double Shot > FMOD > Patcher > Event to GUID");
+            }
+            else if (check.Contains(PatchContent.PatchReferenceAppend) && !EditorPrefs.HasKey("DoubleShot.FMODGUIDTweak.Enabled"))
+            {
+                if (EditorUtility.DisplayDialog("Double Shot Audio Patcher", "Event to GUID patch detected, but registry key for settings not found.\n\n" +
+                        "Do you want to enable using GUID by default when selecting events on FMOD Event Browser?\n\n" +
+                        "You can always change the setting in Double Shot > FMOD > Use GUID as Event Path.", "I want to use GUID as reference", "I want to use Path as reference"))
+                {
+                    EditorPrefs.SetBool("DoubleShot.FMODGUIDTweak.Enabled", true);
+                }
+                else
+                    EditorPrefs.SetBool("DoubleShot.FMODGUIDTweak.Enabled", false);
+
+            }
+        }
+    }
+    public class Patcher
+    {
+        [MenuItem("Double Shot/FMOD/Patcher/Event to GUID", false, 0)]
+        public static void EventToGUIDPatcher()
         {
             string eventRefDrawerPath = Path.Combine(Application.dataPath, PatchContent.EventRefDrawerPath);
             string eventBrowserPath = Path.Combine(Application.dataPath, PatchContent.EventBrowserPath);
@@ -100,26 +153,31 @@ namespace DoubleShot.Editor
                 return;
             }
 
+            // Check if swap patch is already applied
+            string check = File.ReadAllText(eventRefDrawerPath);
+            if (check.Contains(PatchContent.PatchReferenceAppend))
+            {
+                Debug.Log("Event to GUID already patched!");
+                return;
+            }
 
             if (EditorUtility.DisplayDialog("Double Shot Audio Patcher",
                 "This FMOD patch will add a \"Swap\" button to [FMODUnity.EventRef] property which facilitates swapping Event Path with GUID. \n\n" +
+                "In addition, this patch will allow you to use GUID by default when selecting events on FMOD Event Browser. \n\n" +
                 "Patch has been tested and verified in FMOD Version 2.00.08 up to 2.01.07. \n\n" +
                 "Do you want to continue?", "Yes", "No"))
             {
                 // Check if swap patch is already applied
-                string check = File.ReadAllText(eventRefDrawerPath);
                 if (check.Contains(PatchContent.PatchReferenceAppend))
                 {
-                    Debug.Log("Event to GUID Swapper Patch already applied.");
+                    Debug.Log("EventRefDrawer.cs already patched!");
                     return;
                 }
-
-                EditorPrefs.SetBool("DoubleShot.FMODGUIDTweak.Enabled", false);
 
                 // reading all the file and then put it into temp string
                 string temp = File.ReadAllText(eventRefDrawerPath);
                 temp = temp.Insert(0, PatchContent.PatchReferenceAppend); // Putting reference that the file has been patched
-                temp = temp.Replace(PatchContent.EventRefDrawerSearchString, PatchContent.EventRefDrawerReplaceString);
+                temp = temp.Replace(PatchContent.DrawerSearch, PatchContent.DrawerReplace);
                 temp = temp.Replace(PatchContent.SwapperSearch, PatchContent.SwapperReplace);
                 File.WriteAllText(eventRefDrawerPath, temp);
 
@@ -127,15 +185,18 @@ namespace DoubleShot.Editor
                 check = File.ReadAllText(eventBrowserPath);
                 if (check.Contains(PatchContent.PatchReferenceAppend))
                 {
-                    Debug.Log("Event to GUID Workflow Patch already applied.");
+                    Debug.Log("EventBrowser.cs already patched!");
                     return;
                 }
 
                 temp = File.ReadAllText(eventBrowserPath);
                 temp = temp.Insert(0, PatchContent.PatchReferenceAppend); // Putting reference that the file has been patched
-                temp = temp.Replace(PatchContent.EventBrowserSearchStringOne, PatchContent.EventBrowserReplaceStringOne);
-                temp = temp.Replace(PatchContent.EventBrowserSearchStringTwo, PatchContent.EventBrowserReplaceStringTwo);
+                temp = temp.Replace(PatchContent.BrowserSearchOne, PatchContent.BrowserReplaceOne);
+                temp = temp.Replace(PatchContent.BrowserSearchTwo, PatchContent.BrowserReplaceTwo);
+                temp = temp.Replace(PatchContent.BrowserSearchThree, PatchContent.BrowserReplaceThree);
                 File.WriteAllText(eventBrowserPath, temp);
+
+                EditorPrefs.SetBool("DoubleShot.FMODGUIDTweak.Enabled", true);
 
                 AssetDatabase.ImportAsset("Assets/Plugins/FMOD/src/Editor/EventRefDrawer.cs");
                 AssetDatabase.ImportAsset("Assets/Plugins/FMOD/src/Editor/EventBrowser.cs");
@@ -147,9 +208,9 @@ namespace DoubleShot.Editor
 
     public class EnableDisableTweak
     {
-        private const string MenuName = "Double Shot/FMOD/Tweak Enabled";
+        private const string MenuName = "Double Shot/FMOD/Use GUID as Event Path";
         private const string SettingName = "DoubleShot.FMODGUIDTweak.Enabled";
-        
+
         public static bool isTweakEnabled
         {
             get { return EditorPrefs.GetBool(SettingName, false); }
@@ -165,9 +226,31 @@ namespace DoubleShot.Editor
         [MenuItem(MenuName, true)]
         private static bool ToggleTweakValidate()
         {
-            Menu.SetChecked(MenuName, isTweakEnabled);
-            return true;
+            if (!EditorPrefs.HasKey(SettingName))
+                return false;
+            else
+            {
+                Menu.SetChecked(MenuName, isTweakEnabled);
+                return true;
+            }
         }
 
+        // For debugging only
+        [MenuItem("Double Shot/Delete Key")]
+        private static void DeleteKey()
+        {
+            EditorPrefs.DeleteKey(SettingName);
+        }
+        [MenuItem("Double Shot/Set Key")]
+        private static void SetKey()
+        {
+            EditorPrefs.SetBool(SettingName, true);
+        }
+        [MenuItem("Double Shot/Force Editor Reload")]
+        private static void ForceReload()
+        {
+            AssetDatabase.ImportAsset("Assets/Plugins/FMOD/src/Editor/EventRefDrawer.cs");
+        }
     }
+
 }
